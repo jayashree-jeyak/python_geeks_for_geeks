9bbaedefaf48bdd5993d157f510f11e9153befaa
jayashree-jeyak <jayashreejeyak@gmail.com>
2025-01-28 21:08:29 +0530

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,5 @@
+principal=float(input("enter the principal amount:"))
+rate=float(input("enter the rate:"))
+time=int(input("enter the time amount(in years):"))
+simple_interest=(principal*rate*time)/100
+print("The Simple interest for the principal amount",principal, "is:",simple_interest)
